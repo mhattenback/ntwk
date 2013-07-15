c0f60f48316b5eb3b3b6ee884a9d96e0c6ebae69
Max Hattenback <max.hattenback@gmail.com>
2013-07-15 17:31:26 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ntwk::Application.config.secret_token = '118cd79e400821fc0db8088f25fce59b0163df2d92c5383a8307feb1433d6d1f78ae2f442ab9aa5189cc84162db1026aa0a76b9ef8c4fb7b10d1d36bb5eb06e6'
